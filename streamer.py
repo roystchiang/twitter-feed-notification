8f697da1ed01de42b1a92b2cb454e789f387ee01
Roy <crosscent@gmail.com>
2017-03-04 11:25:49 -0800

    Add simple offline tweet mimick

--- streamer.py
+++ streamer.py
@@ -3,19 +3,25 @@ import multiprocessing
 import settings
 import tweepy
 
-class Streamer(multiprocessing.Process):
+class Streamer(object):
     """
     Worker for streaming a given user's tweets
     """
 
-    def __init__(self, file_name, offline=False):
-        multiprocessing.Process.__init__(self)
-        self.offline = offline
-        self.file_name = file_name
+    def __init__(self, offline_tweet=None):
+        self.offline_tweet = offline_tweet
+
+    def _offline_runner(self):
+        while True:
+            tweet = self.offline_tweet.get_tweet()
+            if tweet:
+                print tweet
+            else:
+                break
 
     def run(self):
-        proc_name = self.name
-        print "Streaming {}".format(self.offline)
+        if self.offline_tweet:
+            self._offline_runner()
 
 
 class OfflineTweets(object):
@@ -26,9 +32,6 @@ class OfflineTweets(object):
         self.file_name = file_name
         self.tweets = []
         self.initialize_tweets()
-        for tweet in self.tweets:
-            print tweet['created_at']
-
 
     def initialize_tweets(self):
         with open(self.file_name) as f:
@@ -37,9 +40,15 @@ class OfflineTweets(object):
                 self.tweets.append(data)
         self.tweets = sorted(self.tweets, key=lambda tweet: tweet['created_at'])
 
+    def get_tweet(self):
+        if len(self.tweets):
+            return self.tweets.pop(0)
+        else:
+            return None
+
 
 if __name__ == "__main__":
     print "Now streaming tweets"
-    streamer = Streamer(True)
     offline_tweets = OfflineTweets("tweets.json")
-    streamer.start()
+    streamer = Streamer(offline_tweets)
+    streamer.run()
